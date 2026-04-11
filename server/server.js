5abe0c325c06f45a47a2448b65127e0af26f182a
Sai Krishnan <saikrishnanmk1603@gmail.com>
2026-04-11 07:11:07 +0530

    Implement manual CORS middleware to resolve persistent Vercel errors

--- server/server.js
+++ server/server.js
@@ -18,12 +18,20 @@ const PORT = process.env.PORT || 5000;
 app.use(compression());
 socketManager.init(server);
 
-app.use(cors({
-  origin: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
-  credentials: true,
-}));
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (origin) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
