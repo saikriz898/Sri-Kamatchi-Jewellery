43631a8285ac338c572d3f4c214ab4d8f450f66d
Sai Krishnan S <saikrishnan.s2024cse@sece.ac.in>
2026-05-08 06:59:56 +0530

    fix: optimize db pool for neon auto-wake

--- server/db.js
+++ server/db.js
@@ -2,7 +2,12 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: { 
+    rejectUnauthorized: false 
+  },
+  max: 10, // Neon free tier limit
+  connectionTimeoutMillis: 10000, // Wait 10s for compute to wake up
+  idleTimeoutMillis: 30000,
 });
 
 module.exports = pool;
